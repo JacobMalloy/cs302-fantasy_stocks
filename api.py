60b62e4eb85dc20a27ceef859e7b4e426cb46ccf
Jacob Malloy <jmalloy1@vols.utk.edu>
2021-11-20 18:56:52 -0500

    make plot use get for players

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
+from flask.wrappers import Request
 import matplotlib.pyplot as plt
 import numpy as np
 import io
 import random
 from flask import Flask
+import flask
 from flask import Response
 from flask_cors import CORS
 import pandas as pd
@@ -13,12 +15,14 @@ from matplotlib.figure import Figure
 app = Flask(__name__)
 CORS(app)
 
-csv_files=dict()
-data_frames=dict()
 data = dict()
 players = dict()
 
 def setup():
+    global players
+    global data
+    csv_files=dict()
+    data_frames=dict()
     #gets all of the csv files.
     for year in range(1999,2021):
         for week in range(1,18):
@@ -36,15 +40,16 @@ def setup():
             if s not in players:
                 players[s] = dict()
                 players[s]["start"]=i.split("-")[0]
-                players[s]["Pos"]=row["Pos"]
+                #players[s]["Pos"]=row["Pos"]
             players[s]["end"]=i.split("-")[0]
             p = row["PPRFantasyPoints"]
             data[i][s]=p
+    players={key: value for key, value in sorted(players.items(), key=lambda item: item[0])}
 
-    
+
 
 @app.route("/get_players/", methods=['GET', 'POST'])
-def get_players():#get the names of players and return a json file of their name...   
+def get_players():#get the names of players and return a json file of their name...
     json_object=json.dumps(players, indent=4)
     print(json_object)
     return json_object
@@ -52,20 +57,19 @@ def get_players():#get the names of players and return a json file of their name
 @app.route('/testing/', methods=['GET', 'POST'])
 def welcome():
     return pd.read_csv("./data/2019/week6.csv").to_json()
-    
+
 @app.route('/plot.png/')
 def plot_png():
-    fig = create_figure()
+    fig = create_figure(flask.request.args.get("player").replace("%20"," "))
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-def create_figure():
+def create_figure(player):
 
     playerData=dict()
     score =0.0
     upto="2020-17"
-    player="Peyton Manning"
     for i in data:
         if i==upto:
             break
@@ -80,7 +84,7 @@ def create_figure():
 
     fig = plt.figure(figsize=(12, 6))
 
-    ax = fig.add_subplot(121)
+    ax = fig.add_subplot(111)
 
 
     x, y = zip(*lists) # unpack a list of pairs into two tuples
